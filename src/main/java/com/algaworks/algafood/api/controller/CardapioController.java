9600bce2dc80852d2ee45b646aad3e0ec2a16272
joao.sipauba <joao.sipauba@ceuma.br>
2022-01-10 01:25:32 -0300

    Criando documentação com swagger

--- src/main/java/com/algaworks/algafood/api/controller/CardapioController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CardapioController.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.model.Item;
 import com.algaworks.algafood.domain.repository.ItemRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +16,19 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Api(value = "Cardapio")
 public class CardapioController {
 
     @Autowired
     private ItemRepository itemRepository;
 
+        @ApiOperation(value = "Lista todos os items do cardapio")
         @GetMapping("/cardapio")
         public List<Item> listarItens(){
             return itemRepository.findAll();
         }
 
+        @ApiOperation(value = "Lista um item especifico do cardapio")
         @GetMapping("/cardapio/{itemId}")
         public ResponseEntity<Item> buscarItem(@PathVariable Long itemId){
             Optional<Item> item = itemRepository.findById(itemId);
@@ -35,12 +40,14 @@ public class CardapioController {
             return ResponseEntity.notFound().build();
         }
 
+        @ApiOperation(value = "Cria um novo item para o cardapio")
         @PostMapping("/cardapio")
         @ResponseStatus(HttpStatus.CREATED)
         public Item criarItem(@RequestBody Item item){
             return itemRepository.save(item);
         }
 
+        @ApiOperation(value = "Atualiza um item do cardapio")
         @PutMapping("/cardapio/{itemId}")
         public ResponseEntity<Item> atualizaItem(@PathVariable Long itemId, @RequestBody Item item ){
 
@@ -54,6 +61,7 @@ public class CardapioController {
             return ResponseEntity.ok(item);
         }
 
+        @ApiOperation(value = "Exclui um item do cardapio")
         @DeleteMapping("/cardapio/{itemId}")
         public ResponseEntity<Item> excluiItem(@PathVariable Long itemId){
             if (!itemRepository.existsById(itemId)){
